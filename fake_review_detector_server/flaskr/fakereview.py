e58b6e491519cf8e2e56d56e6e9e49a09b059be6
Yazhi Chen <yazhi.fz@gmail.com>
2021-05-09 13:40:32 +1000

    Integration landingpage
    Integration landingpage

--- fake_review_detector_server/flaskr/fakereview.py
+++ fake_review_detector_server/flaskr/fakereview.py
@@ -18,6 +18,7 @@ import math
 from flask_login import LoginManager, UserMixin
 import requests             
 from bs4 import BeautifulSoup
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -99,6 +100,10 @@ def about():
 def howtouse():
     return render_template("how-to-use.html")
 
+@app.route("/howitworks")
+def howitworks():
+    return render_template("How-It-Works.html")
+
 @app.route("/contact")
 def contact():
     return render_template("contact-us.html")
@@ -149,6 +154,72 @@ def predict(y_prob=None):
                            perc_fake_review = round((1-float(true_reviews_count/(total_reviews_count+0.01)))*100,2)
                            )
 
+@app.route('/predict/api/v1', methods=['POST'])
+def predict_api():
+    """
+    Do prediction with one review from front-end in the style of RESTfuL.
+    """
+    if request.method == 'POST':
+        message = request.form['message']
+        data = [message]
+        # to count words in string
+        print(message)
+        res = len(re.findall(r'\w+', message))
+
+        # document level language detection. Think of it like average language of the document!
+        nlp = spacy.load('resources/en_core_web_sm')
+        # nlp.add_pipe(LanguageDetector(), name='language_detector', last=True)
+        nlp.add_pipe('language_detector', last=True)
+        doc = nlp(message)
+        main_language = doc._.language["language"]
+        print(main_language)
+
+        # the word limitation
+        my_prediction = [0]
+        y_prob_deceptive = 0
+        if res >= 20 and main_language == "en":
+            my_prediction = clf.predict(cv.transform(data).toarray())
+            y_prob = clf.predict_proba(cv.transform(data).toarray()) #return the labe prediction probability
+            y_prob_deceptive = y_prob[:,1]*100 #label prediction probability in percent
+            db.insert_review(mysql,message, my_prediction[0])
+    
+    total_reviews_count, true_reviews_count = db.get_review_stat(mysql)
+    return jsonify({
+                    'word_count': res,
+                    'language': main_language,
+                    'prediction': str(my_prediction[0])
+                    })
+
+def extract_one_pagination(node):
+  """
+  """
+  p = {}
+  p['name'] = node.string
+  if 'disabled' in node['class']:
+      p['status'] = 'disable'
+  else:
+      p['status'] = None
+  if 'href' in node.attrs:
+      p['url'] = 'https://www.tripadvisor.com.au' + node['href']
+  else:
+      p['url'] = '#'
+  print(p)
+  return p
+
+def extract_paginations(content):
+  """
+  """
+  ps = []
+  paging = content.findAll('div',attrs={"class":"ui_pagination"})
+  for c in paging[0].children:
+    if 'pageNumbers' in c['class']:
+        for cc in c.children:
+            ps.append(extract_one_pagination(cc))
+    else:
+        ps.append(extract_one_pagination(c))
+  print(ps)
+  return ps
+
 def scrape(start_url):
   """
   Crawl the given start_url.
@@ -164,6 +235,9 @@ def scrape(start_url):
   url_parts = start_url.split('-Reviews-')
   urls = []
   reviewArr = []
+  biz_type = None
+  platform_type = None
+  paginations = None
   if "Hotel_Review" in start_url:
       url = start_url
       # print(url)
@@ -181,6 +255,9 @@ def scrape(start_url):
                   "review_date": review.find('div', attrs={"class": "_2fxQ4TOx"}).text}
               print(reviewObject)
               reviewArr.append(reviewObject)
+              biz_type = "Hotel"
+              platform_type = "TripAdvisor"
+          paginations = extract_paginations(content)
 
   elif "Restaurant_Review" in start_url:
       url = start_url
@@ -200,9 +277,43 @@ def scrape(start_url):
               }
               print(reviewObject)
               reviewArr.append(reviewObject)
+              biz_type = "Restaurant"
+              platform_type = "TripAdvisor"
+          paginations = extract_paginations(content)
+
+  elif "www.yelp.com" in start_url:
+    biz_type = "Biz"
+    platform_type = "Yelp"
+    if "?start=" not in start_url:
+        # Only access the first page.
+        for page in range(0, 1):
+            url = start_url + '?start={}'.format(10*page)
+            print(url)
+            urls.append(url)
+    else:
+        for page in range(int(int(start_url[-2:])/10), int(int(start_url[-2:])/10 + 5)):
+            url = start_url[:-9] + '?start={}'.format(10*page)
+            print(url)
+            urls.append(url)
+    for url in urls:
+        response = requests.get(url,timeout=10)
+        try:
+            status = response.status_code
+            print(status)
+        except Exception as e:
+            print(e)
+        content = BeautifulSoup(response.content,"html.parser")
+        for review in content.findAll('div',{"class":"review__373c0__13kpL border-color--default__373c0__2oFDT"}):
+            reviewObject = {
+                "review": review.find('p', attrs={"class": "comment__373c0__1M-px css-n6i4z7"}).text.replace("\xa0", ""),
+                "review_rating": review.select('[aria-label*=rating]')[0]['aria-label'][:1],
+                "review_date": review.find('span', attrs={"class": "css-e81eai"}).text
+                }
+            print(reviewObject)
+            reviewArr.append(reviewObject)
   else: 
       print("Please only paste URL of hotel or restaurant review from Trip Advisor!")
-  return reviewArr
+  return biz_type, platform_type, reviewArr, paginations
 
 
 @app.route('/results',methods=['POST'])
@@ -211,16 +322,23 @@ def results():
     Do prediction with one url from pront-end.
     """
     if request.method == 'POST':
+      # testing
+      scrape_data = [{'review_title': 'Quality food, great service. ', 'review': 'We had a couple of pizzas and the fruitti da mare pasta. All delicious. They catered for my gluten intolerances and my fussy 6yo’s requirements. Good wine selection too. Highly recommend.', 'review_rating': '5', 'date_of_visit': 'April 2021', 'review_date': 'Reviewed 6 days ago'},{'review_title': '2 glasses of Wild Turkey $36 1 Spaghetti of the Sea $35 what a blatant ripoff.', 'review': 'Food was good, staff were nice, but to charge $12 a nip of Wild Turkey and $6 for a small bottle of coke, and pour it yourself, who the hell does that???Yes that is right $18 for a nip of Wild Turkey in a...small to medium glass and add your own coke and ice, - twice.  I asked for a Wild Turkey and coke in a tall glass with ice, go figure.More', 'review_rating': '2', 'date_of_visit': 'April 2021', 'review_date': 'Reviewed 4 weeks ago'},{'review_title': 'Supreme dinner!', 'review': 'We booked this restaurant on a Monday night and everything was amazing! We had starters, pizza and pasta and as we are Italians I can tell you that this is the best restaurant I’ve been in Melbourne! Great location and quiet spot! Be back 100%....Many thanks to the owners and all the staff.More', 'review_rating': '5', 'date_of_visit': 'March 2021', 'review_date': 'Reviewed March 22, 2021'},{'review_title': 'Disappointed', 'review': 'I booked this restaurant as they advertise freshly made pasta however what we received was chewy, bland and very disappointing.The entree focaccia with tomatoes and buffalo mozzarella was yummy though and the pizzas looked good - we should have ordered them instead.My husband...gave the feedback that he was disappointed in the quality of the pasta and instead of accepting the feedback the staff turned very defensive and aggressive with a cook coming out of the kitchen with a snap lock bag of this supposed fresh pasta to prove to us it was fresh however it made a clanking sound when dropped on the bench very much like dry/packet pasta sounds. We were also surprised that there wasn’t a manager to speak to on a Saturday night.More', 'review_rating': '2', 'date_of_visit': 'March 2021', 'review_date': 'Reviewed March 21, 2021'},{'review_title': 'Sensational ', 'review': 'Excellent night out. Pasta and pizzas was tasty, piping hot and prices quite reasonable. Tucked away down Little Collins St', 'review_rating': '5', 'date_of_visit': 'March 2021', 'review_date': 'Reviewed March 20, 2021'},{'review_title': 'Top class', 'review': 'Excellent location. Beautifully presented dining space. Excellent pizza with buffalo mozzarella and a good wine choice.Prices not cheap but reasonable for the quality.Service top notch.', 'review_rating': '5', 'date_of_visit': 'March 2021', 'review_date': 'Reviewed March 20, 2021'},{'review_title': 'Delish dinner date', 'review': 'Had a fantastic dinner date. Enjoyed the calzone with extra hot salami and a few glasses of pinot grigio.', 'review_rating': '5', 'date_of_visit': 'March 2021', 'review_date': 'Reviewed March 17, 2021'},{'review_title': 'Delicious pasta', 'review': 'I was walking by this place and thought it looked nice so I decided to try a few dishes - am so glad I did. The service was very prompt and the Matriciana and Tiramisu were very tasty. I’ll definitely return!', 'review_rating': '4', 'date_of_visit': 'March 2021', 'review_date': 'Reviewed March 15, 2021'},{'review_title': 'Venetian Gem', 'review': "Tucked away out of the busy restaurant precincts of Melbourne, Da Guido 365 is a hidden gem. My. wife and I decided to share our meal and we enjoyed every morsel.We ordered some Maccheroni all' Amatriciana, Tortellini panna, prosciutto e funghi, as well as...a Da Guido pizza, along with a glass of Chianti.Before our meals were served, we were presented with a delicious, complimentary bruschetta, which fired up our tastebuds for the main event.Let me say that this was one of the finest Italian meals I have eaten in Melbourne. The pasta was perfectly 'al dente' and the pizza, with a delicious base was outstanding. The restaurant had a pleasant, welcoming 'buzz' and the service was informative, friendly and genuine. I would not hesitate to recommend Da Guido 365 to anyone seeking a genuine, Italian experience.More", 'review_rating': '5', 'date_of_visit': 'March 2021', 'review_date': 'Reviewed March 14, 2021'},{'review_title': 'Disappointing experience, small serve, food cold', 'review': "First off, this review is for my satisfaction of how Da Guido handled my home delivery. I placed an order for ravioli and received a call from a strange mobile number a relatively short time later. The mobile number was the restaurant saying the food...was ready. The caller asked me to contact the delivery service (no restaurant has ever asked me to do this before) but anyways I did. I then still had no food an hour later so I called the restaurant's mobile number back. They assured me they would follow up and call me back but I received no call. I felt like the restaurant was again passing all of the responsibility onto me when all I did was make the order. Then to add insult to injury, a stone cold ravioli turns up. What made it worse after waiting over 2 hours was that it was about 10 squares of pasta for $35. I might be being generous. While the restaurant cannot handle what the delivery service does, I find the restaurant's poor level of customer service, pricing and serving quantity to be absolutely inadequate.More", 'review_rating': '1', 'date_of_visit': 'March 2021', 'review_date': 'Reviewed March 13, 2021'}]
+      paginations = [{'name': 'Previous', 'status': 'disable', 'url': None}, {'name': 'Next', 'status': None, 'url': 'https://www.tripadvisor.com/Restaurant_Review-g255100-d728473-Reviews-or10-Sud_Food_and_Wine-Melbourne_Victoria.html'}, {'name': '1', 'status': None, 'url': 'https://www.tripadvisor.com/Restaurant_Review-g255100-d728473-Reviews-Sud_Food_and_Wine-Melbourne_Victoria.html'}, {'name': '2', 'status': None, 'url': 'https://www.tripadvisor.com/Restaurant_Review-g255100-d728473-Reviews-or10-Sud_Food_and_Wine-Melbourne_Victoria.html'}, {'name': '3', 'status': None, 'url': '/Restaurant_Review-g255100-d728473-Reviews-or20-Sud_Food_and_Wine-Melbourne_Victoria.html'}, {'name': '4', 'status': None, 'url': '/Restaurant_Review-g255100-d728473-Reviews-or30-Sud_Food_and_Wine-Melbourne_Victoria.html'}, {'name': '5', 'status': None, 'url': '/Restaurant_Review-g255100-d728473-Reviews-or40-Sud_Food_and_Wine-Melbourne_Victoria.html'}, {'name': '6', 'status': None, 'url': '/Restaurant_Review-g255100-d728473-Reviews-or50-Sud_Food_and_Wine-Melbourne_Victoria.html'}, {'name': '…', 'status': None, 'url': None}, {'name': '21', 'status': None, 'url': '/Restaurant_Review-g255100-d728473-Reviews-or200-Sud_Food_and_Wine-Melbourne_Victoria.html'}]
 
       url = request.form['urlinput']
       print("The requested url: " + url)
-      data = []
-      try:
-        scrape_data = scrape(url)
-        data = [d['review'] for d in scrape_data]
-      except BaseException:
-        print("Scrape timeout!")
-      # print(data)
+      # data = []
+      # try:
+      #   biz_type, platform_type, scrape_data, paginations = scrape(url)
+      #   data = [d['review'] for d in scrape_data]
+      # except BaseException:
+      #   print("Scrape timeout!")
+
+      # testing
+      biz_type = 'Restaurant'
+      platform_type = 'TripAdvisor'
+      data = [d['review'] for d in scrape_data]
 
       shop_name = ''
       if len(data) >= 1:
@@ -230,11 +348,19 @@ def results():
         y_prob_deceptive_4_fake = y_prob[:,0]*100 #label prediction probability in percent
         print(y_prob)
         try:
-          shop_names = ((url.split('-Reviews-'))[1].split('-'))
-          if len(shop_names) == 2:
-            shop_name = shop_names[0]
-          elif len(shop_name) == 3:
-            shop_name = shop_names[1]
+          if platform_type == 'TripAdvisor':
+            shop_names = ((url.split('-Reviews-'))[1].split('-'))
+            if len(shop_names) == 2:
+              shop_name = shop_names[0]
+            elif len(shop_name) == 3:
+              shop_name = shop_names[1]
+          elif platform_type == 'Yelp':
+            shop_names = ((url.split('/biz/'))[1].split('-'))
+            if len(shop_names) == 2:
+              shop_name = shop_names[0]
+            elif len(shop_name) >= 3:
+              for idx in range(len(shop_names)-1):
+                shop_name += shop_names[idx]
         except BaseException:
           print("Failed to extract shop_name from url!", url)
       else:
@@ -250,14 +376,19 @@ def results():
       # The thredhold for fake probability to tell one is a fake.
       thredhold = 65
       for i in range(len(data)):
-        ret = {
-                "review_title":scrape_data[i]["review_title"],
-                "review_rating":scrape_data[i]["review_rating"],
-                "review_date":scrape_data[i]["review_date"],
-                "review":data[i]
-                # "deceptive_prob":str(round(y_prob_deceptive[i],2))+'%'
-              }
-        # if my_prediction[i] == 0:
+        if 'review_title' in scrape_data[i]:
+          ret = {
+                  "review_title":scrape_data[i]["review_title"],
+                  "review_rating":scrape_data[i]["review_rating"],
+                  "review_date":scrape_data[i]["review_date"],
+                  "review":data[i]
+                }
+        else:
+          ret = {
+                  "review_rating":scrape_data[i]["review_rating"],
+                  "review_date":scrape_data[i]["review_date"],
+                  "review":data[i]
+                }
         if y_prob_deceptive_4_fake[i] > thredhold:
           ret['deceptive_prob'] = str(round(y_prob_deceptive_4_fake[i],2))+'%'
           fake_ret.append(ret)
@@ -276,25 +407,26 @@ def results():
                             fake_ret = fake_ret,
                             not_fake_ret = not_fake_ret,
                             fake_reviews_count = fake_reviews_count,
-                            true_reviews_count = true_reviews_count
+                            true_reviews_count = true_reviews_count,
+                            biz_type = biz_type,
+                            platform_type = platform_type,
+                            paginations = paginations
                           )
 
 
 if __name__ == '__main__':
 
     env = app.config['ENV']
+    # test
+    env = 'Debug'
     print("Loading in {} environment ....".format(env))
 
     # Read resources
-    #filename = 'resources/CatBoostClassifier.pkl'
-    #filename = 'resources/lightGBM.pkl'
-    #clf = pickle.load(open(filename,'rb'))
     clf = bz2.BZ2File(open('resources/LGBM.pkl.pbz2', 'rb'))
     clf = cPickle.load(clf)
 
     cv = bz2.BZ2File(open('resources/cvtransform.pkl.pbz2', 'rb'))
     cv = cPickle.load(cv)
-    #cv = pickle.load(open('resources/transform.pkl', 'rb'))
 
     # Read config.init
     config = configparser.ConfigParser()
---
+++ fake_review_detector_server/flaskr/static/js/myvalidator.js
@@ -0,0 +1,30 @@
+$(function () {
+    $('form').bootstrapValidator({
+        message: 'This value is not valid',
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            urlinput: {
+                message: '',
+                validators: {
+                    notEmpty: {
+                        message: 'Please enter an URL.'
+                    },
+                    regexp: {
+                        // regexp: /(www.tripadvisor.com.*\/(Hotel_Review-|Restaurant_Review-))|(www.yelp.com.*)/g,
+                        regexp: /www.yelp.com.*/g,
+                        message: 'Please Enter a solid URL for Tripadvisor or Yelp.'
+                    },
+                    regexp: {
+                        regexp: /www.tripadvisor.com.*\/(Hotel_Review-|Restaurant_Review-)/g,
+                        // regexp: /www.yelp.com.*/g,
+                        message: 'Please Enter a solid URL for Tripadvisor or Yelp.'
+                    }
+                }
+            }
+        }
+    });
+});
